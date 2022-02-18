6c0ffe2c4d6ecfca8632d27601d91040012c6201
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-19 01:03:59 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -15,7 +15,7 @@ jpegThumbnail: fs.readFileSync('./gambar1.jpeg')
 }
 let listOwner = new Array()
   for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
-  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://github.com/Johannes2803\nEMAIL;Email Owner: johannes123.j1@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://github.com/Luigmntng\nEMAIL;Email Owner: Lu vVibu yh Vanh ?\nORG: BUKAN BOT + BUTKON \nTEL;NO CHAT P;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
   }
   let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: fakegrup })
           let buttons = [

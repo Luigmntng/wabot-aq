9e548106fd98f3eb03d08d5d832d24f1cc625d35
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-19 00:59:03 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,37 @@
-function handler(m) {
-  // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+let fs = require('fs')
+let handler = async function (m) {
+	const fakegrup = {
+	key : {
+fromMe: false,
+participant : '0@s.whatsapp.net',
+remoteJid: 'status@broadcast'
+},
+message: {
+locationMessage: {
+name: 'Japan',
+jpegThumbnail: fs.readFileSync('./gambar1.jpeg')
+  }
+ }
+}
+let listOwner = new Array()
+  for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://github.com/Johannes2803\nEMAIL;Email Owner: johannes123.j1@gmail.com\nORG: BUKAN BOT + NO SAVE\nTEL;NO CHAT P;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  }
+  let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: fakegrup })
+          let buttons = [
+  {buttonId: '#donasi', buttonText: {displayText: 'DONASI'}, type: 1},
+  {buttonId: '#menu', buttonText: {displayText: 'BACK TO MENU'}, type: 1},
+  {buttonId: '#sc', buttonText: {displayText: 'SOURCE CODE'}, type: 1},  
+  
+]
+const buttonsMessage = {
+    contentText: `
+Hai Kak @${m.sender.replace(/@.+/, '')} 👋\nNih Kak Kontak Owner Saya Jangan Di Spam Kak
+`.trim(),    footerText: `${conn.getName(conn.user.jid)}`,
+    buttons: buttons,
+  headerType: 'EMPTY'
+}
+conn.sendMessage(m.chat, buttonsMessage, 'buttonsMessage', { contextInfo: { mentionedJid: [m.sender]}})
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']

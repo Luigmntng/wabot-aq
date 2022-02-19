c414040db88df32fd4546a4f361c98833d54d2dd
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-20 00:23:25 +0700

    Create sakatonic.js

---
+++ plugins/sakatonic.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://restapi-production-a62b.up.railway.app/api/wallpaper/satanic?apikey=APIKEY`)).buffer(), 'Nih kak', footer, 'Bg Lu ateis ?', `.estetikwallpaper`, m)
+
+}
+
+handler.help = ['sakatonicwallpaper']
+handler.tags = ['wall']
+handler.command = /^(sakatonicwallpaper)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
+

930c7ee3a9f936477c1b9d790418042c7e9af610
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-19 22:19:07 +0700

    Update estetik.js

--- plugins/estetik.js
+++ plugins/estetik.js
@@ -1,1 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://restapi-production-a62b.up.railway.app/api/wallpaper/wallhp?apikey=APIKEY`)).buffer(), 'Nih kak', footer, 'Makasih Bot >///<', `.estetikwallpaper`, m)
+
+}
+
+handler.help = ['estetikwallpaper']
+handler.tags = ['wall']
+handler.command = /^(estetikwallpaper)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
 

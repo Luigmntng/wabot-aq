85efc52d65536b25772681cbaf3759d5cd820892
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-21 01:41:04 +0700

    Update tebakkata_ans.js

--- plugins/tebakkata_ans.js
+++ plugins/tebakkata_ans.js
@@ -9,12 +9,12 @@ module.exports = {
         if (m.quoted.id == this.tebakkata[id][0].id) {
             let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
             // m.reply(JSON.stringify(json, null, '\t'))
-            if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
+            if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
                 global.db.data.users[m.sender].exp += this.tebakkata[id][2]
                 m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP`)
                 clearTimeout(this.tebakkata[id][3])
                 delete this.tebakkata[id]
-            } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+            } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
             else m.reply(`*Salah!*`)
         }
         return !0

df219a85dd04c3e374a9172d1a3029bd60eca0df
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-20 20:01:20 +0700

    Update wg.js

--- plugins/wg.js
+++ plugins/wg.js
@@ -1,1 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://restapi-production-a62b.up.railway.app/api/wallpaper/gaming?apikey=APIKEY`)).buffer(), 'Udah kak Jgn Lupa Follow ig @ahmdlui', footer, 'iya bangh hehe', `.gamingwall`, m)
 
+}
+
+handler.help = ['gamingwall']
+handler.tags = ['wall']
+handler.command = /^(gamingwall)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler

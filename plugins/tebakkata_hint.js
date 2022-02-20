67ffcddb3dd2324f76ee7180e09bbe6fb3dccf8e
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-21 01:47:13 +0700

    Update tebakkata_hint.js

--- plugins/tebakkata_hint.js
+++ plugins/tebakkata_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakkata)) throw false
     let json = conn.tebakkata[id][1]
-    let ans = json.result.jawaban.trim()
+    let ans = json.jawaban.trim()
     let clue = ans.replace(/[AIUEO]/g, '_')
     m.reply('```' + clue + '```')
 }

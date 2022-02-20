e3c14701ee6b864bcd97d167fe3e16c7767a8f60
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-20 12:52:55 +0700

    Update wallislami.js

--- plugins/wallislami.js
+++ plugins/wallislami.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, usedPrefix }) => {
 	
-	conn.sendButtonImg(m.chat, await ( await fetch(`https://restapi-production-a62b.up.railway.app/api/wallpaper/islami?apikey=APIKEY`)).buffer(), 'Nih kak', footer, 'Subhanallah (>-<)', `.estetikwallpaper`, m)
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://restapi-production-a62b.up.railway.app/api/wallpaper/islami?apikey=APIKEY`)).buffer(), 'Nih kak Jgn Lupa solat dan Juga Follow ig @ahmdlui :v', footer, 'Subhanallah (>-<)', `.wallislami`, m)
 
 }
 

e0359d3a6f86200331b2827200221db2bb7a5ca4
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-24 00:09:30 +0700

    Update Weabu.js

--- plugins/Weabu.js
+++ plugins/Weabu.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 		}, 'buttonsMessage', { quoted: m })
 	} else throw json
 }
-handler.help = [ 'yotsuba', 'shinomiya', 'yumeko', 'tejina', 'chiho', 'boruto', 'kaori', 'shizuka', 'kaga', 'kotori', 'mikasa', 'akiyama', 'gremory', 'isuzu', 'shina', 'kagura', 'shinka', 'eba', 'elaina', 'yuri', 'erza', 'hinata', 'minato', 'Naruto', 'Sagiri', 'nezuko', 'rize', 'Anna', 'Deidara', 'Yuki', 'Anna', 'Asuna', 'Ayuzawa', 'Chitoge', 'Emilia', 'hestia', 'inori', 'itachi', 'Madara', 'sakura', 'Sasuke', 'Tsunade', 'onepiece']
-handler.tags = ['wibu']
+handler.help = ['yotsuba', 'shinomiya', 'yumeko', 'tejina', 'chiho', 'boruto', 'kaori', 'shizuka', 'kaga', 'kotori', 'mikasa', 'akiyama', 'gremory', 'isuzu', 'shina', 'kagura', 'shinka', 'eba', 'elaina', 'yuri', 'erza', 'hinata', 'minato', 'Naruto', 'Sagiri', 'nezuko', 'rize', 'Anna', 'Deidara', 'Yuki', 'Anna', 'Asuna', 'Ayuzawa', 'Chitoge', 'Emilia', 'hestia', 'inori', 'itachi', 'Madara', 'sakura', 'Sasuke', 'Tsunade', 'onepiece']
+handler.tags = ['internet']
 handler.command = /^(yotsuba|shinomiya|yumeko|tejina|chiho|boruto|kaori|shizuka|kaga|kotori|mikasa|akiyama|gremory|isuzu|shina|kagura|shinka|eba|elaina|erza|hinata|minato|naruto|sagiri|nezuko|rize|anna|deidara|yuki|anna|asuna|ayuzawa|chitoge|emilia|hestia|inori|itachi|madara|sakura|sasuke|tsunade|onepiece)$/i
 module.exports = handler

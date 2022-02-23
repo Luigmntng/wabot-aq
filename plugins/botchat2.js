266acbd104de87957eb0cef7cedc1ffa7de75565
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-23 19:50:54 +0700

    Update botchat2.js

--- plugins/botchat2.js
+++ plugins/botchat2.js
@@ -23,7 +23,7 @@ var seconds = Math.floor(seconds % 60);
 return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
 }
 runtime = process.uptime()
-					teks = `𝙍𝙚𝙖𝙡 𝙁𝙖𝙢𝙨 𝙆𝙞𝙡𝙡𝙚𝙧`// \n\n${kyun(runtime)}`
+					teks = `𝑪𝒆𝒄𝒂𝒌𝑩𝒐𝒕𝒛`// \n\n${kyun(runtime)}`
 					run = `${kyun(runtime)}`
 					var itsme = `0@s.whatsapp.net`
 					var split = `teks`

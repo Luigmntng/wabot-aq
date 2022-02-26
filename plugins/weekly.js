d291a02ea67d7626c22f914d3b489660de59a344
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-26 15:20:25 +0700

    Add files via upload

--- plugins/weekly.js
+++ plugins/weekly.js
@@ -1,4 +1,4 @@
-  let { MessageType } = require('@adiwajshing/baileys')
+let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]

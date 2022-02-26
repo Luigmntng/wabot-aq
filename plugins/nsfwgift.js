0f43d3b03e3dec3e30fe6a6e8bd6beafbe78ddd8
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-26 10:44:10 +0700

    Create nsfwgift.js

---
+++ plugins/nsfwgift.js
@@ -0,0 +1,23 @@
+let fetch = require("node-fetch")
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn }) => {
+  let res = await fetch(global.API('https://restapi-production-a62b.up.railway.app/api', '/nsfw/gifs?apikey=APIKEY'))
+  let json = await res.json()
+  let {
+  result
+  } = json
+  let stiker = await sticker(null, result, global.packname, global.author)
+  if (stiker) return conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+  throw stiker.toString()
+}
+handler.help = ['nsfwgift']
+handler.tags = ['premium']
+handler.customPrefix = /^nsfwgift$/i
+handler.command = new RegExp
+
+module.exports = handler
+

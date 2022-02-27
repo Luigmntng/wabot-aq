e6de4f61f2844b0d05f77379033561566e5aa58e
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-27 20:49:25 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -15,19 +15,19 @@ jpegThumbnail: fs.readFileSync('./gambar1.jpeg')
 }
 let listOwner = new Array()
   for (let i of owner.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')) {
-  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://github.com/Luigmntng\nEMAIL;Email Owner: Lu vVibu yh Vanh ?\nORG: BUKAN BOT + BUTKON \nTEL;NO CHAT P;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
+  	listOwner.push({ vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${this.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Work\nURL;Web gwejh: https://doujindesu.xxx\nEMAIL;Email Owner: Haisayang@hengker.com\nORG: WAJIB SAVE KHUSUS CWE\nTEL;Senang bisa membantumu;waid= ${i.split('@')[0]}:${i.split('@')[0]}\nEND:VCARD` })
   }
   let send = this.sendMessage(m.chat, { displayName: listOwner.length + ' kontak', contacts: listOwner }, 'contactsArrayMessage', { quoted: fakegrup })
           let buttons = [
-  {buttonId: '#donasi', buttonText: {displayText: 'DONASI'}, type: 1},
-  {buttonId: '#menu', buttonText: {displayText: 'BACK TO MENU'}, type: 1},
-  {buttonId: '#sc', buttonText: {displayText: 'SOURCE CODE'}, type: 1},  
+  {buttonId: '#donasi', buttonText: {displayText: 'Minta Uangnya Om'}, type: 1},
+  {buttonId: '#menu', buttonText: {displayText: 'BACK '}, type: 1},
+  {buttonId: '#sc', buttonText: {displayText: 'SC 😱😱'}, type: 1},  
   
 ]
 const buttonsMessage = {
     contentText: `
-Hai Kak @${m.sender.replace(/@.+/, '')} 👋\nNih Kak Kontak Owner Saya Jangan Di Spam Kak
-`.trim(),    footerText: `${conn.getName(conn.user.jid)}`,
+Hai Kak @${m.sender.replace(/@.+/, '')} 👋\nNih Kak Kontak Owner Saya Kalo Mau Jadi Pcrnya Chat Yh (>\\\<) 
+`.trim(),    footerText: `${NOTE : khusus cewe}`,
     buttons: buttons,
   headerType: 'EMPTY'
 }

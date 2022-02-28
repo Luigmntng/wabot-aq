e5077728c101d69ff2855aed4ed1a6ddfc7cc9b7
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-28 10:21:14 +0700

    Update nekopai.js

--- plugins/nekopai.js
+++ plugins/nekopai.js
@@ -7,7 +7,10 @@ let handler = async (m, { conn, args }) => {
   conn.sendFile(m.chat, res, 'neko.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
-handler.tags = ['nulis']
+handler.tags = ['wibu']
 handler.command = /^(logoneko)$/i
+
+handler.limit = true
+handler.register = true
 
 module.exports = handler

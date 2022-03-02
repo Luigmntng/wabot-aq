fe1df36cf6a872d9e964e6426ff702e599d2c806
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-02 20:42:57 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,17 +1,28 @@
+let handler = m => m
 let levelling = require('../lib/levelling')
-module.exports = {
-	before(m) {
-		let user = global.db.data.users[m.sender]
-		if (!user.autolevelup) return !0
-		let before = user.level * 1
-		while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-
-		if (before !== user.level) {
-			m.reply(`
-Selamat, anda telah naik level!
-*${before}* -> *${user.level}*
-gunakan *.profile* untuk mengecek
-	`.trim())
-		}
-	}
+handler.before = m => {
+    let user = global.db.data.users[m.sender]
+    if (!user.autolevelup) return
+    if (m.sender === global.conn.user.jid) return
+    let before = user.level * 1
+    while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+        let nani = 'https://telegra.ph/file/04e443fbac9d2393f36e0.jpg'
+  await conn.sendButtonLoc(m.chat, await (await fetch(nani)).buffer(), `
+╭─❑ 「 Level Up 」 ❑─
+╠➥*Nama* : ${conn.getName(m.sender)}
+╠➥*Role* :%role
+╰*Level* : *${before}* ➥ *${user.level}*
+`.trim(), '', 'Profile', '.profile')
+ false, {
+            contextInfo: {
+                mentionedJid: [m.sender]
+            }
+        })
+    }
+    return true
 }
+ 
+module.exports = handler
+
+
